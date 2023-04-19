94f3933f0bd3dddcdad1993c2004cebc3e58cec7
Carlos ALvarez <carlosmail1978@gmail.com>
2023-04-19 21:10:00 +0200

    Add component and types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,8 +9,10 @@ module.exports = {
       extends: ["xo-typescript", "prettier"],
       files: ["*.ts", "*.tsx"],
       rules: {
-        "@typescript-eslint/consistent-type-exports": ["error", "interface"],
-        "no-ununused-vars": "error",
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
       },
     },
   ],
