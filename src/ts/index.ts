90340492b1d2bf239fd4157a4c50b423ce4eef53
Carlos ALvarez <carlosmail1978@gmail.com>
2023-04-20 19:35:42 +0200

    Add AppComponent component

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,10 @@
+import { AppComponent } from "./components/AppComponent/AppComponent.js";
 import { CardComponent } from "./components/CardComponent/CardComponent.js";
 import series from "./series/series.js";
 import { type CardStructure } from "./types/types.js";
+
+const appContainer = document.querySelector("body")!;
+new AppComponent(appContainer);
 
 const container = document.querySelector(".series");
 series.forEach((serie: CardStructure) => new CardComponent(container!, serie));
