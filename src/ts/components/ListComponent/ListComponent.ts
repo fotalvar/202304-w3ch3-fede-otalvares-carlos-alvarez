fe08b6d66f068f95ebd6b87c944fed5f9e9b350d
Fede <fnotalvares@gmail.com>
2023-04-20 21:32:03 +0200

    Add ListWatchedComponent to ListComponent.ts

--- src/ts/components/ListComponent/ListComponent.ts
+++ src/ts/components/ListComponent/ListComponent.ts
@@ -1,3 +1,4 @@
+import series from "../../series/series.js";
 import Component from "../Component/Component.js";
 
 export class ListComponent extends Component {
@@ -13,6 +14,24 @@ export class ListComponent extends Component {
         <span class="list__info">You have 5 series pending to watch</span>
         <!--<span class="list__info">Congrats! You've watched all your series</span>-->
         <ul class="series">
+        </ul>
+      </section>`;
+  }
+}
+
+export class ListWatchedComponent extends Component {
+  constructor(parentElement: Element) {
+    super(parentElement, "section", "list");
+    this.renderHtml();
+  }
+
+  renderHtml() {
+    this.element.innerHTML = `
+      <section class="watched-list">
+        <h3 class="list__title">Watched series</h3>
+        <span class="list__info">You have 5 series pending to watch</span>
+        <!--<span class="list__info">Congrats! You've watched all your series</span>-->
+        <ul class="watched-series series">
         </ul>
       </section>`;
   }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,9 @@
 import { AppComponent } from "./components/AppComponent/AppComponent.js";
 import { CardComponent } from "./components/CardComponent/CardComponent.js";
-import { ListComponent } from "./components/ListComponent/ListComponent.js";
+import {
+  ListComponent,
+  ListWatchedComponent,
+} from "./components/ListComponent/ListComponent.js";
 import series from "./series/series.js";
 import { type CardStructure } from "./types/types.js";
 
@@ -12,3 +15,13 @@ new ListComponent(sectionList);
 
 const container = document.querySelector(".series");
 series.forEach((serie: CardStructure) => new CardComponent(container!, serie));
+
+const sectionListWatched = document.querySelector("h2")!;
+new ListWatchedComponent(sectionListWatched);
+
+const watchedList = document.querySelector(".watched-series");
+series.forEach((serie: CardStructure) => {
+  if (serie.isWatched) {
+    new CardComponent(watchedList!, serie);
+  }
+});
