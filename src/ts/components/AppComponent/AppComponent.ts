fe361cf7f3dfde185b93393639f066b0fea28f2f
Fede <fnotalvares@gmail.com>
2023-04-20 20:08:07 +0200

    Add instance of  ListComponent to index.ts

--- src/ts/components/AppComponent/AppComponent.ts
+++ src/ts/components/AppComponent/AppComponent.ts
@@ -11,6 +11,9 @@ export class AppComponent extends Component {
         <header class="main-header">
       <h1 class="main-title">My Series</h1>
     </header>
+    <main class="main-content">
+    <h2 class="main-content__title">Series list</h2>
+    </main>
     `;
   }
 }
--- src/ts/components/ListComponent/ListComponent.ts
+++ src/ts/components/ListComponent/ListComponent.ts
@@ -1,14 +1,13 @@
 import Component from "../Component/Component.js";
 
-class ListComponent extends Component {
+export class ListComponent extends Component {
   constructor(parentElement: Element) {
-    super(parentElement, "main", "main-content");
+    super(parentElement, "section", "list");
     this.renderHtml();
   }
 
   renderHtml() {
     this.element.innerHTML = `
-    <h2 class="main-content__title">Series list</h2>
       <section class="list">
         <h3 class="list__title">Pending series</h3>
         <span class="list__info">You have 5 series pending to watch</span>
